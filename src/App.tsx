668e1e1d66276626a4c50f3c47b416aa1922c1df
Inka Elijoki <inka.elijoki@tutors.es>
2023-06-23 17:11:38 +0300

    taustakuva

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import "./index.css";
+import React from "react";
+import mtt_kuva from "./mtt_kuva.png";
 
-function App() {
-  const [count, setCount] = useState(0)
+console.log(mtt_kuva);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+function Header() {
+  return <img src={mtt_kuva} alt="mtt_kuva" />;
 }
 
-export default App
+export default Header;
